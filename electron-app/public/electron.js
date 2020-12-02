0f13f254816c600b30605686a58a217c79e71954
Joseph Johny Emmatty <josinho.seven@gmail.com>
2020-12-02 07:20:21 +0530

    integrate electron js

---
+++ electron-app/public/electron.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const electron = require("electron");
+const isDev = require("electron-is-dev");
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 900, height: 680 });
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  mainWindow.maximize();
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
