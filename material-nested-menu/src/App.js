402b543f3aa708da003de97342f0c75b93fd8938
Joseph Johny Emmatty <josinho.seven@gmail.com>
2020-12-11 07:19:18 +0530

    corrected menu item structure

--- material-nested-menu/src/App.js
+++ material-nested-menu/src/App.js
@@ -9,14 +9,14 @@ const items = [
   {
     label: "Menu 3",
     children: [
+      { label: "Menu 3.1" },
       {
-        label: "Menu 3.1",
-        children: [{ label: "Menu 3.1.1" }, { label: "Menu 3.1.2" }],
+        label: "Menu 3.2 long item",
+        children: [{ label: "Menu 3.2.1" }, { label: "Menu 3.2.2" }],
       },
-      { label: "Menu 3.2" },
       { label: "Menu 3.3" },
       {
-        label: "Menu 3.4 long item",
+        label: "Menu 3.4",
         children: [{ label: "Menu 3.4.1" }, { label: "Menu 3.4.3" }],
       },
     ],
@@ -50,6 +50,7 @@ const items = [
       },
     ],
   },
+  { label: "Menu 6" },
 ];
 
 const App = () => {
