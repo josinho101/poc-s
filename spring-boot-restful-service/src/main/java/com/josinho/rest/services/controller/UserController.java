fb36d47ceb787d8b9494fa4cbeade280a5d16664
Joseph Johny Emmatty <josinho.seven@gmail.com>
2021-12-06 06:28:44 +0530

    Added Get and Post request for user

--- spring-boot-restful-service/src/main/java/com/josinho/rest/services/controller/UserController.java
+++ spring-boot-restful-service/src/main/java/com/josinho/rest/services/controller/UserController.java
@@ -1,30 +1,49 @@
 package com.josinho.rest.services.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.josinho.rest.services.dao.UserDaoService;
+import com.josinho.rest.services.exceptions.UserNotFoundException;
 import com.josinho.rest.services.modals.User;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-	
+
 	@Autowired
 	private UserDaoService userService;
 
 	@GetMapping
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return userService.findAll();
 	}
-	
+
 	@GetMapping("/{id}")
 	public User findOne(@PathVariable int id) {
-		return userService.findOne(id);
+		User user = userService.findOne(id);
+		if (user == null) {
+			throw new UserNotFoundException("id - " + id);
+		}
+
+		return user;
+	}
+
+	@PostMapping
+	public ResponseEntity<Object> save(@RequestBody User user) {
+		User newUser = userService.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newUser.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
 	}
 }
