ece462c8aa7d6165cde12382765e5d19dcdf53fd
Joseph Johny Emmatty <josinho.seven@gmail.com>
2022-02-19 07:35:37 +0530

    Added EF Sample

---
+++ EFDemoApp/EFDataAccessLibrary/Migrations/20220219012908_CorrectedZipCodeColumnName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFDataAccessLibrary.Migrations
+{
+    public partial class CorrectedZipCodeColumnName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ZipcCode",
+                table: "Addresses",
+                newName: "ZipCode");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ZipCode",
+                table: "Addresses",
+                newName: "ZipcCode");
+        }
+    }
+}
---
+++ EFDemoApp/EFDataAccessLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFDataAccessLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
