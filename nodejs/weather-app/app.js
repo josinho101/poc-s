793fc743fa84a74cfcd27b8ec78680a029cc579e
Joseph <josinho.seven@gmail.com>
2022-04-07 18:44:59 +0530

    Added error handling for API calls

--- nodejs/weather-app/app.js
+++ nodejs/weather-app/app.js
@@ -2,11 +2,16 @@ const axios = require("axios");
 
 axios.default
   .get(
-    "http://api.weatherstack.com/current?access_key=89d0b3895084082f2b278ac64810967a&query=India&units=m"
+    "http://api.weatherstack.com/current?access_key=89d0b3895084082f2b278ac64810967a&query=Neyyattinkara&units=m"
   )
   .then((res) => {
-    const { temperature, feelslike, weather_descriptions } = res.data.current;
-    console.log(
-      `${weather_descriptions}. It is currently ${temperature} degress out. It feels like ${feelslike} degress out.`
-    );
-  });
+    if (res.data.error) {
+      console.error("No data available!");
+    } else {
+      const { temperature, feelslike, weather_descriptions } = res.data.current;
+      console.log(
+        `${weather_descriptions}. It is currently ${temperature} degress out. It feels like ${feelslike} degress out.`
+      );
+    }
+  })
+  .catch((error) => console.error("Something gone bad!!" + error));
