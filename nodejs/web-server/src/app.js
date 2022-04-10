2f9bf8b250946ae52118c81f13fe6e9565d29760
Joseph <josinho.seven@gmail.com>
2022-04-10 08:06:58 +0530

    Added partials

--- nodejs/web-server/src/app.js
+++ nodejs/web-server/src/app.js
@@ -1,26 +1,31 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "../templates"));
+app.set("views", path.join(__dirname, "../templates/views"));
+hbs.registerPartials(path.join(__dirname, "../templates/partials"));
 
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
   res.render("index", {
-    title: "Weather app Index",
-    name: "Kungfu panda",
+    title: "Weather Home",
+    name: "Weather app",
   });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", {
+    title: "About page",
+  });
 });
 
 app.get("/help", (req, res) => {
   res.render("help", {
+    title: "Help page",
     message:
       "This is a sample message. If you need a real help contact adminstrator!!",
   });
@@ -30,6 +35,20 @@ app.get("/weather", (req, res) => {
   res.send({
     forcast: "It's sunny",
     location: "India",
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "Help page",
+    error: "Help article not found!",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "Not found!",
+    error: "Page you are looking for dosen't exist",
   });
 });
 
