6c727abf419cb342a164678c177c089ace5def4c
Joseph <josinho.seven@gmail.com>
2022-04-19 21:40:56 +0530

    Added evn files and properties

--- nodejs/task-manager/src/models/user.js
+++ nodejs/task-manager/src/models/user.js
@@ -67,9 +67,13 @@ userSchema.virtual("tasks", {
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
 
-  const token = jwt.sign({ _id: user._id.toString() }, "thisismynewcourse", {
-    expiresIn: "30 minutes",
-  });
+  const token = jwt.sign(
+    { _id: user._id.toString() },
+    process.env.JWT_PRIVATE_KEY,
+    {
+      expiresIn: "30 minutes",
+    }
+  );
   user.tokens = user.tokens.concat({ token });
   await user.save();
 
