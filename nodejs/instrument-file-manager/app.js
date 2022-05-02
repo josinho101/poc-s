de13f53dc16e26361b083abb25e5ce5007ab7af5
Joseph <josinho.seven@gmail.com>
2022-05-02 16:23:40 +0530

    Added date field for data generator

--- nodejs/instrument-file-manager/app.js
+++ nodejs/instrument-file-manager/app.js
@@ -3,34 +3,45 @@ const { v4: uuidv4 } = require("uuid");
 const File = require("./modals/file");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/InstrumentFileManager", {
+  .connect("mongodb://127.0.0.1:27017/FileStore", {
     useNewUrlParser: true,
   })
   .then(async () => {
     console.log("Connected to database");
     console.time("total-time");
 
-    const fileCount = 100;
-    const files = [];
+    const fileCount = 1000000;
+    const multiplier = 10;
+    let files = [];
     const exts = ["jpg", "png", "L01", "001", "txt"];
 
     await File.deleteMany();
 
-    console.time("Iteration-time");
+    for (let i = 0; i < multiplier; i++) {
+      console.time("Iteration-time");
 
-    for (let i = 0; i < fileCount; i++) {
-      const instrumentId = getRandom(1, 5);
-      const ext = exts[instrumentId - 1];
-      const filename = `${uuidv4()}.${ext}`;
-      const file = new File({ instrumentId, filename, ext });
-      files.push(file);
+      for (let i = 0; i < fileCount; i++) {
+        const instrumentId = getRandom(1, 5);
+        const ext = exts[instrumentId - 1];
+        const filename = `${uuidv4()}.${ext}`;
+        const file = new File({
+          instrumentId,
+          filename,
+          ext,
+          date: Date.now(),
+        });
+        files.push(file);
+        // await file.save();
+      }
+
+      console.timeEnd("Iteration-time");
+
+      console.time("Save-time");
+      await File.insertMany(files);
+      console.timeEnd("Save-time");
+
+      files = [];
     }
-
-    console.timeEnd("Iteration-time");
-
-    console.time("Save-time");
-    await File.insertMany(files);
-    console.timeEnd("Save-time");
 
     console.timeEnd("total-time");
     console.log("Database save completed!");
--- nodejs/instrument-file-manager/modals/file.js
+++ nodejs/instrument-file-manager/modals/file.js
@@ -5,10 +5,11 @@ const file = new mongoose.Schema(
     instrumentId: { type: Number, required: true },
     filename: { type: String, required: true },
     ext: { type: String, required: true },
+    date: { type: Date, required: true },
   },
   { timestamps: true }
 );
-file.index({ instrumentId: 1, ext: 1 });
+file.index({ instrumentId: 1, ext: 1, date: 1 });
 
 const File = mongoose.model("File", file);
 
