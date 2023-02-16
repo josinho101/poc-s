35d3be42db9095169fa77134e3fdc3fd566bc92e
Joseph Johny Emmatty <josinho.seven@gmail.com>
2023-02-16 22:58:50 +0530

    Sample to draw triangle using webgl

---
+++ webgl-sample/triangle/webgl.js
@@ -0,0 +1,65 @@
+const canvas = document.getElementById("canvas");
+const gl = canvas.getContext("webgl");
+
+if (!gl) {
+  console.error("WebGL not supported!!");
+}
+
+const vertexData = [0, 1, 0, 1, -1, 0, -1, -1, 0]; // triangle data
+const colorData = [1, 0, 0, 0, 1, 0, 0, 0, 1];
+
+const vBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+const cBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
+
+const vShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(
+  vShader,
+  `
+    precision mediump float;
+    attribute vec3 position;
+    attribute vec3 color;
+    varying vec3 vColor;
+    void main() {
+      vColor = color;
+      gl_Position = vec4(position, 1);
+    }
+  `
+);
+gl.compileShader(vShader);
+
+const fShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(
+  fShader,
+  `
+    precision mediump float;
+    varying vec3 vColor;
+    void main() {
+      gl_FragColor = vec4(vColor, 1);
+    }
+  `
+);
+gl.compileShader(fShader);
+
+const program = gl.createProgram();
+gl.attachShader(program, vShader);
+gl.attachShader(program, fShader);
+
+gl.linkProgram(program);
+
+const positionLocation = gl.getAttribLocation(program, "position");
+gl.enableVertexAttribArray(positionLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+const colorLocation = gl.getAttribLocation(program, "color");
+gl.enableVertexAttribArray(colorLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
+
+gl.useProgram(program);
+gl.drawArrays(gl.TRIANGLES, 0, 3);
